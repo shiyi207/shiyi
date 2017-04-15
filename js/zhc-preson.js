0739d5c5e9569c11b92133a5b087cb92d06660ad
Mrzhanghc <>
2017-04-15 20:51:40 +0800

    '改善消息列表，新增签到页面'

--- js/zhc-preson.js
+++ js/zhc-preson.js
@@ -4,4 +4,15 @@ $(document).ready(function(){
 //	$('.zhc-list ul').children().first().children().last().text($('.zhc-list ul li').length)
 //	console.log($('.zhc-list ul'))
 	
+//	$('#search').keyup(function(){
+//		let value=$(this).val()
+//		$('.zhc-list ul').filter('.zhc-le-top span').text('333')
+//	})
+	
+	
+//	$('.zhc-li-right').addEventListener('touchmove',function(){
+//		$('.zhc-li-right p').animate({
+//			right:'2rem',
+//		})
+//	})
 })
